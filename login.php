c70bf23c35f15ca58642a72dd82958ac748d94ae
AamishAnsari <44465275+AamishAnsari@users.noreply.github.com>
2018-11-28 23:00:51 +0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> sign in| sign up</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="header">
+	<h2>Login</h2>
+</div>
+
+ <form method="post" action="login.php">
+    <div class="input-group">
+    	<label>Usename</label>
+    	<input type="text" name="username">
+    </div>
+   
+    <div class="input-group">
+    	<label>Password</label>
+    	<input type="password" name="password_1">
+    </div>
+     <div class="input-group">
+    	<button type="submit" name="login" class="btn">Login</button>
+    </div>
+    <p>
+    	Not yet a member? <a href="register.php">Sign up</a>
+    </p>
+ </form>
+</body>
+</html>
